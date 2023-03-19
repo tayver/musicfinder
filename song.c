808836a38a1372e8e0cc546250b91e262b68c20f
tayver <k001229@g.skku.edu>
2023-03-19 10:40:36 +0900

    Create song.c

---
+++ song.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	double l, h;
+	char m;
+
+	printf("희망 주파수 값을 입력해주세요:(예시: 27.5 4186)");
+	scanf("%f %f", &l, &h);
+	printf("기본 조를 입력해주세요:(예시: ");
+
+	return 0;
+}
